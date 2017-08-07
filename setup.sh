cff83b639366d86ef57688d18c021dfba87cb604
myukiando <30616695+myukiando@users.noreply.github.com>
2017-08-07 10:22:46 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update -y
 suod apt-get upgrade -y
 sudo apt-get install -y zsh vim 
-chsh chsh -s /usr/bin/zsh
+chsh -s /usr/bin/zsh
 
 chmod 775 ~/dotfiles/setup.sh
 ~/dotfiles/set_dotfiles.sh
